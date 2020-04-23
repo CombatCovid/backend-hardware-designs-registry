16b71edb7b2bf3f787f25688bf6cff1233d64218
DNature <divinehycenth@outlook.com>
2020-04-23 07:42:09 +0100

    Added express-session and redis

--- src/components/user/resolvers.ts
+++ src/components/user/resolvers.ts
@@ -4,7 +4,6 @@ import { ResolverMap } from '../../types';
 export const resolvers: ResolverMap = {
   Mutation: {
     signUp: async (_, { username, ...args }: GQL.ISignUpOnMutationArguments, context): Promise<any> => {
-      context.session.userId = 'Hello world';
       console.log(context.req.session);
       const user = await UserModel.getUser({ username });
       if (user) {
