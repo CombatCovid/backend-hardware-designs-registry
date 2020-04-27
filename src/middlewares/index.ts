32a85bab8d6fb07fd60ad504e4355f53d604be8d
Rahul <rahul.singh@smartsensesolutions.com>
2020-04-28 01:17:22 +0530

    added file in gitignore

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,12 +2,16 @@ import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as cors from 'cors';
 import { Application } from 'express';
+import passport from './passport';
+import session from './session';
 
 class Middlewares {
   public init(app: Application) {
     app.use(bodyParser.json());
     app.use('*', cors());
     app.use(compression());
+    session(app);
+    passport(app);
   }
 }
 
